1244c54ebd9c0a393bfc6fb28bf78f5bbbac8dfe
Phill <philltv.gaming@gmail.com>
2023-01-24 00:24:59 +0100

    uploaded assets

--- Website/website/vite.config.ts
+++ Website/website/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000
+  },
+  build: {
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 2000
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL("./src", import.meta.url)),
+      '@components': fileURLToPath(new URL("./src/components", import.meta.url))
+    }
+  }
 })
